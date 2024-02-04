f42b60134a2539dbc49582721ab75bcc23d0aacc
xan-asc <119889093+Xan-Asc@users.noreply.github.com>
2024-02-04 04:08:59 -0800

    Reorganize files and moved includes to xml files for easier merges

--- PastLoot/Locales/Locale-enUS.lua
+++ PastLoot/Locales/Locale-enUS.lua
@@ -85,9 +85,9 @@ L["Unknown Filters"] = true
 L["Up"] = true
 L["Will roll greed on all loot matching this rule."] = true
 L["Will roll need on all loot matching this rule."] = true
-L["ItemInSet"] = true
 
 local LM = LibStub("AceLocale-3.0"):NewLocale("PastLoot_Modules", "enUS", true)
+LM["ItemInSet"] = true
 LM["In A Set"] = true
 LM["Not In A Set"] = true
 LM["Owned"] = true
